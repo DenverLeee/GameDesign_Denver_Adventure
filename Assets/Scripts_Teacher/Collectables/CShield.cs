a6d215a589b8a9b4db1b059ac228d33f9a6a0d70
Denver <1139643975@qq.com>
2022-01-02 11:02:24 +0800

    Add Tutorial Scripts

---
+++ Assets/Scripts_Teacher/Collectables/CShield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CShield : Collectables
+{
+    [SerializeField] private int shieldToAdd = 1;
+    [SerializeField] private ParticleSystem shieldEffect;
+
+    protected override void Pick()
+    {
+        AddShield(character);
+    }
+
+    protected override void PlayEffects()
+    {
+        Instantiate(shieldEffect, transform.position, Quaternion.identity);       
+    }
+
+    public void AddShield(Character characterHealth)
+	{
+		characterHealth.GetComponent<Health>().GainShield(shieldToAdd);  
+    }
+}
